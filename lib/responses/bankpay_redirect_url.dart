pokepay/flutter-sdk

commit 51b7a626be78f3ce9decca444bc770e44893fc8c

Support custom domain

lib/responses/private_money.dart
@@ -12,7 +12,7 @@ class PrivateMoney {
   final String type;
   final String unit;
   final String description;
-  final String onelineMessage;
+  final String onlineMessage;
   final String? accountImage;
   final Images images;
   final Organization organization;
@@ -25,6 +25,7 @@ class PrivateMoney {
   final String? paymentActUrl;
   final String? commercialActUrl;
   final bool canUseCreditCard;
+  final String? customDomainName;
 
   PrivateMoney({
     required this.id,
@@ -32,7 +33,7 @@ class PrivateMoney {
     required this.type,
     required this.unit,
     required this.description,
-    required this.onelineMessage,
+    required this.onlineMessage,
     this.accountImage,
     required this.images,
     required this.organization,
@@ -45,6 +46,7 @@ class PrivateMoney {
     this.paymentActUrl,
     this.commercialActUrl,
     required this.canUseCreditCard,
+    this.customDomainName,
   });
 
   factory PrivateMoney.fromJson(Map<String, dynamic> json) => _$PrivateMoneyFromJson(json);

lib/responses/private_money.g.dart
@@ -12,11 +12,10 @@ PrivateMoney _$PrivateMoneyFromJson(Map<String, dynamic> json) => PrivateMoney(
       type: json['type'] as String,
       unit: json['unit'] as String,
       description: json['description'] as String,
-      onelineMessage: json['oneline_message'] as String,
+      onlineMessage: json['oneline_message'] as String,
       accountImage: json['account_image'] as String?,
       images: Images.fromJson(json['images'] as Map<String, dynamic>),
-      organization:
-          Organization.fromJson(json['organization'] as Map<String, dynamic>),
+      organization: Organization.fromJson(json['organization'] as Map<String, dynamic>),
       maxBalance: (json['max_balance'] as num?)?.toDouble(),
       transferLimit: (json['transfer_limit'] as num?)?.toDouble(),
       expirationType: json['expiration_type'] as String,
@@ -26,16 +25,16 @@ PrivateMoney _$PrivateMoneyFromJson(Map<String, dynamic> json) => PrivateMoney(
       paymentActUrl: json['payment_act_url'] as String?,
       commercialActUrl: json['commercial_act_url'] as String?,
       canUseCreditCard: json['can_use_credit_card'] as bool,
+      customDomainName: json['custom_domain_name'] as String?,
     );
 
-Map<String, dynamic> _$PrivateMoneyToJson(PrivateMoney instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$PrivateMoneyToJson(PrivateMoney instance) => <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
       'type': instance.type,
       'unit': instance.unit,
       'description': instance.description,
-      'oneline_message': instance.onelineMessage,
+      'online_message': instance.onlineMessage,
       'account_image': instance.accountImage,
       'images': instance.images.toJson(),
       'organization': instance.organization.toJson(),
@@ -48,4 +47,5 @@ Map<String, dynamic> _$PrivateMoneyToJson(PrivateMoney instance) =>
       'payment_act_url': instance.paymentActUrl,
       'commercial_act_url': instance.commercialActUrl,
       'can_use_credit_card': instance.canUseCreditCard,
+      'custom_domain_name': instance.customDomainName,
     };

lib/bank_api/private_money.dart
@@ -1,7 +1,8 @@
+import 'package:pokepay_flutter_sdk/responses.dart';
 import 'package:pokepay_flutter_sdk/responses/paginated_coupons.dart';
 
 import '../pokepay_sdk.dart';
-import '../responses.dart';
+import '../responses/private_money.dart' as response;
 
 extension PrivateMoney on PokepayAPI {
   Future<PaginatedPrivateMoneys> searchPrivateMoneys({
@@ -42,6 +43,20 @@ extension PrivateMoney on PokepayAPI {
         'before': before,
         'after': after,
         'perPage': perPage,
+      },
+    );
+  }
+
+  Future<response.PrivateMoney> getPrivateMoney({
+    required String privateMoneyId,
+  }) async {
+    return await invokeMethod<response.PrivateMoney>(
+      (j) => response.PrivateMoney.fromJson(j),
+      "getPrivateMoney",
+      {
+        'env': this.env.index,
+        'accessToken': this.accessToken,
+        'privateMoneyId': privateMoneyId,
       },
     );
   }

android/src/main/java/jp/pokepay/pokepay_sdk/PokepaySdkPlugin.java
@@ -47,6 +47,7 @@ import jp.pokepay.pokepaylib.BankAPI.Check.GetCheck;
 import jp.pokepay.pokepaylib.BankAPI.Check.UpdateCheck;
 import jp.pokepay.pokepaylib.BankAPI.CpmToken.GetCpmToken;
 import jp.pokepay.pokepaylib.BankAPI.Account.PatchAccountCouponDetail;
+import jp.pokepay.pokepaylib.BankAPI.PrivateMoney.GetPrivateMoney;
 import jp.pokepay.pokepaylib.BankAPI.PrivateMoney.GetPrivateMoneyCoupons;
 import jp.pokepay.pokepaylib.BankAPI.PrivateMoney.SearchPrivateMoneys;
 import jp.pokepay.pokepaylib.BankAPI.Terminal.AddTerminalPublicKey;
@@ -101,6 +102,7 @@ import jp.pokepay.pokepaylib.Responses.PaginatedCoupons;
 import jp.pokepay.pokepaylib.Responses.PaginatedMessages;
 import jp.pokepay.pokepaylib.Responses.PaginatedPrivateMoneys;
 import jp.pokepay.pokepaylib.Responses.PaginatedTransactions;
+import jp.pokepay.pokepaylib.Responses.PrivateMoney;
 import jp.pokepay.pokepaylib.Responses.ServerKey;
 import jp.pokepay.pokepaylib.Responses.Terminal;
 import jp.pokepay.pokepaylib.Responses.User;
@@ -278,9 +280,16 @@ public class PokepaySdkPlugin implements FlutterPlugin, MethodCallHandler {
                         Integer expiresIn = call.argument("expiresIn");
                         String accountId = call.argument("accountId");
                         Product[] products = call.argument("products");
-
+                        String privateMoneyId = call.argument("privateMoneyId");
                         Pokepay.setEnv(env);
-                        Pokepay.Client client = new Pokepay.Client(accessToken, null, isMerchant);
+                        Pokepay.Client client;
+
+                        if (privateMoneyId != null){
+                            client = Pokepay.Client.withCustomDomain(accessToken,this.context, isMerchant, privateMoneyId);
+                        }else{
+                            client = new Pokepay.Client(accessToken, null, isMerchant);
+                        }
+
                         String result = client.createToken(amount, description, expiresIn, accountId, products);
                         return new TaskResult(null, result);
                     }
@@ -631,7 +640,15 @@ public class PokepaySdkPlugin implements FlutterPlugin, MethodCallHandler {
                         Pokepay.setEnv(env);
                         String rawStrategy = call.argument("tx_strategy");
                         TransactionStrategy txStrategy = parseTxStrategy(rawStrategy);
-                        Pokepay.Client client = new Pokepay.Client(accessToken, this.context);
+                        String privateMoneyId = call.argument("privateMoneyId");
+                        Pokepay.Client client;
+
+                        if (privateMoneyId != null){
+                            client = Pokepay.Client.withCustomDomain(accessToken,this.context, false, privateMoneyId);
+                        }else{
+                            client = new Pokepay.Client(accessToken, this.context);
+                        }
+
                         UserTransaction userTransaction = client.scanToken(scanToken,amount,accountId,products,
                                 couponId,txStrategy);
                         return new TaskResult(null, userTransaction.toString());
@@ -648,6 +665,15 @@ public class PokepaySdkPlugin implements FlutterPlugin, MethodCallHandler {
                         Pokepay.setEnv(env);
                         PaginatedPrivateMoneys res = req.send(accessToken);
                         return new TaskResult(null, res.toString());
+                    }
+
+                    case "getPrivateMoney":{
+                        Env env = flutterEnvToSDKEnv((int)call.argument("env"));
+                        String accessToken = call.argument("accessToken");
+                        String privateMoneyId = call.argument("privateMoneyId");
+                        Pokepay.setEnv(env);
+                        PrivateMoney privateMoney =  new GetPrivateMoney(privateMoneyId).send(accessToken);
+                        return new TaskResult(null, privateMoney.toString());
                     }
                     case "sendConfirmationEmail": {
                         Env env = flutterEnvToSDKEnv((int)call.argument("env"));

ios/Classes/SwiftPokepaySdkPlugin.swift
@@ -191,15 +191,28 @@ private class MethodCallTask {
         case "createToken":
             let env = flutterEnvToSDKEnv(ienv: args["env"] as! Int32)
             let accessToken = args["accessToken"] as! String
-            let isMerchant = args["isMerchan"] as! Bool
+            let isMerchant = args["isMerchant"] as! Bool
             let amount = args["amount"] as! Double
             let description = args["description"] as! String
             let expiresIn = args["expiresIn"] as? Int32
             let accountId = args["accountId"] as? String
-            let productsString = (args["products"] as! String).data(using: .utf8)!
-            let products = try! BankAPIJSONDecoder().decode([Product]?.self, from: productsString)
-            let client = Pokepay.Client(accessToken: accessToken,isMerchant: isMerchant,env: env)
-            client.createToken(amount,description: description, expiresIn: expiresIn, accountId: accountId, products: products, handler: self.after)
+            let productsString = (args["products"] as? String)?.data(using: .utf8)
+            let products = productsString != nil ? try? BankAPIJSONDecoder().decode([Product]?.self, from: productsString!) : nil
+            let privateMoneyId = args["privateMoneyId"] as? String
+            
+            if privateMoneyId != nil {
+                Pokepay.Client.withCustomDomain(accessToken: accessToken, isMerchant: isMerchant, env: env, challange: privateMoneyId!){ result in
+                    switch result {
+                        case .success(let response):
+                           response.createToken(amount,description: description, expiresIn: expiresIn, accountId: accountId, products: products, handler: self.after)
+                        case .failure(let error):
+                           break
+                    }
+                }
+            }else{
+                let client = Pokepay.Client(accessToken: accessToken,isMerchant: isMerchant,env: env)
+                client.createToken(amount,description: description, expiresIn: expiresIn, accountId: accountId, products: products, handler: self.after)
+            }
         case "createUserTransactionWithBill":
             let env = flutterEnvToSDKEnv(ienv: args["env"] as! Int32)
             let accessToken = args["accessToken"] as! String
@@ -366,6 +379,12 @@ private class MethodCallTask {
             let accessToken = args["accessToken"] as! String
             let client = Pokepay.Client(accessToken: accessToken, env: env)
             client.send(MessagingAPI.GetUnreadCount(), handler: self.after)
+        case "getPrivateMoney":
+            let env = flutterEnvToSDKEnv(ienv: args["env"] as! Int32)
+            let accessToken = args["accessToken"] as! String
+            let client = Pokepay.Client(accessToken: accessToken, env: env)
+            let privateMoneyId = args["privateMoneyId"] as! String
+            client.send(BankAPI.PrivateMoney.GetPrivateMoney(privateMoneyId:privateMoneyId),handler:self.after)
         case "getPrivateMoneyCoupons":
             let env = flutterEnvToSDKEnv(ienv: args["env"] as! Int32)
             let accessToken = args["accessToken"] as! String
@@ -445,11 +464,23 @@ private class MethodCallTask {
             let token = args["scanToken"] as! String
             let amount = args["amount"] as? Double
             let accountId = args["accountId"] as? String
-            let productsString = (args["products"] as! String).data(using: .utf8)!
-            let products = try! BankAPIJSONDecoder().decode([Product]?.self, from: productsString)
+            let productsString = (args["products"] as? String)?.data(using: .utf8)
+            let products = productsString != nil ? try? BankAPIJSONDecoder().decode([Product]?.self, from: productsString!) : nil
             let couponId = args["couponId"] as? String
-            let client = Pokepay.Client(accessToken: accessToken,env: env)
-            client.scanToken(token,amount:amount,accountId:accountId,products:products,couponId:couponId,handler: self.after)
+            let privateMoneyId = args["privateMoneyId"] as? String
+            if privateMoneyId != nil {
+                Pokepay.Client.withCustomDomain(accessToken: accessToken, env: env, challange: privateMoneyId!){ result in
+                    switch result {
+                        case .success(let response):
+                           response.scanToken(token,amount:amount,accountId:accountId,products:products,couponId:couponId,handler: self.after)
+                        case .failure(let error):
+                           break
+                    }
+                }
+            }else{
+                let client = Pokepay.Client(accessToken: accessToken,env: env)
+                client.scanToken(token,amount:amount,accountId:accountId,products:products,couponId:couponId,handler: self.after)
+            }
         case "searchPrivateMoneys":
             let env = flutterEnvToSDKEnv(ienv: args["env"] as! Int32)
             let accessToken = args["accessToken"] as! String

lib/pokepay_sdk.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:logger/logger.dart';
+import 'package:pokepay_flutter_sdk/bank_api/private_money.dart';
 import 'package:pokepay_flutter_sdk/parameters/transaction_strategy.dart';
 
 import 'bank_api/bill.dart';
@@ -141,12 +142,6 @@ const Map<APIEnv, String> envNameMap = {
   APIEnv.DEVELOPMENT: "dev",
 };
 
-String getWebBaseURL(APIEnv env) {
-  String name = envNameMap[env]!;
-  String suffix = ((name.length > 0) ? ("-" + name) : "");
-  return "https://www" + suffix + ".pokepay.jp";
-}
-
 String getAPIBaseURL(APIEnv env) {
   String name = envNameMap[env]!;
   String suffix = ((name.length > 0) ? ("-" + name) : "");
@@ -184,28 +179,38 @@ String parseAsPokeregiToken(String token) {
 
 class PokepayClient {
   PokepayAPI api;
+  String privateMoneyId;
 
   PokepayClient({
     required String accessToken,
     APIEnv env = APIEnv.PRODUCTION,
+    required String privateMoneyId,
   }) : this.api = PokepayAPI(
           accessToken: accessToken,
           env: env,
-        );
+        ),this.privateMoneyId = privateMoneyId;
 
   Future<Terminal> getTerminalInfo() async {
     return this.api.getTerminal();
   }
 
+  Future<String> getWebBaseURL(APIEnv env) async {
+    String name = envNameMap[env]!;
+    String suffix = ((name.length > 0) ? ("-" + name) : "");
+    return (await this.api.getPrivateMoney(privateMoneyId: this.privateMoneyId)).customDomainName ?? "https://www" + suffix + ".pokepay"
+        ".jp";
+  }
+
   Future<TokenInfo> getTokenInfo(String token, APIEnv env) async {
-    if (token.startsWith(getWebBaseURL(env) + "/cashtrays/")) {
+    final webBaseURL = await getWebBaseURL(env);
+    if (token.startsWith(webBaseURL + "/cashtrays/")) {
       return TokenInfo(type: TokenType.CASHTRAY, token: "");
-    } else if (token.startsWith(getWebBaseURL(env) + "/bills/")) {
-      final String uuid = token.substring((getWebBaseURL(env) + "/bills/").length);
+    } else if (token.startsWith(webBaseURL + "/bills/")) {
+      final String uuid = token.substring((webBaseURL + "/bills/").length);
       final bill = await api.getBill(id: uuid);
       return TokenInfo(type: TokenType.BILL, token: bill);
-    } else if (token.startsWith(getWebBaseURL(env) + "/checks/")) {
-      final String uuid = token.substring((getWebBaseURL(env) + "/checks/").length);
+    } else if (token.startsWith(webBaseURL + "/checks/")) {
+      final String uuid = token.substring((webBaseURL + "/checks/").length);
       final check = await api.getCheck(id: uuid);
       return TokenInfo(type: TokenType.CHECK, token: check);
     } else if (RegExp(r'^([0-9A-Z]{25})$').hasMatch(token)) {
@@ -270,7 +275,8 @@ class PokepayClient {
       'description': description,
       'expiresIn': expiresIn,
       'accountId': accountId,
-      'products': products
+      'products': products,
+      'privateMoneyId': this.privateMoneyId
     });
 
     return json;
@@ -293,6 +299,7 @@ class PokepayClient {
       'products': products,
       'couponId': couponId,
       'tx_strategy': strategy.value,
+      'privateMoneyId': this.privateMoneyId
     });
 
     return UserTransaction.fromJson(jsonDecode(json));
@@ -309,6 +316,12 @@ class PokepayOAuthClient {
     required this.clientId,
     required this.clientSecret,
   });
+
+  String getWebBaseURL(APIEnv env) {
+    String name = envNameMap[env]!;
+    String suffix = ((name.length > 0) ? ("-" + name) : "");
+    return "https://www" + suffix + ".pokepay.jp";
+  }
 
   String getAuthorizationUrl({String contact = ""}) {
     String base = getWebBaseURL(this.env) + "/oauth/authorize?client_id=" + this.clientId + "&response_type=code";

commit a21494655ee0ea74d8d66fd4ede9a899054230f3

fix: pin JSON keys clobbered by the clean regeneration
3d753f8 regenerated every .g.dart from a clean build. For four fields the
committed JSON keys changed, because their Dart field names do not round-trip
through json_serializable's `field_rename: snake` (which inserts `_` before
every single uppercase letter):

  PrivateMoney.onlineMessage        oneline_message      -> online_message
  PrivateMoney.canUseC2CTransfer    can_use_c2c_transfer -> can_use_c2_c_transfer
  BankPayRedirectUrl.redirectUrl    redirectUrl          -> redirect_url
  BankPayRedirectUrl.paytreeCustomerNumber
                                    paytreeCustomerNumber -> paytree_customer_number

Both are non-nullable in PrivateMoney, so fromJson now throws on every
response the API actually returns. Pin the keys with @JsonKey so they survive
any future regeneration, and regenerate the two affected files.

`onlineMessage` is a typo of `oneline` introduced in 51b7a62; the field name is
left alone to avoid a breaking API change. Note this also repairs its toJson
side, which had been emitting `online_message` since that commit.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

lib/responses/bankpay_redirect_url.dart
@@ -4,7 +4,11 @@ part 'bankpay_redirect_url.g.dart';
 
 @JsonSerializable()
 class BankPayRedirectUrl {
+  // このエンドポイントのレスポンスキーは snake_case ではなく camelCase のため、
+  // field_rename: snake を @JsonKey で打ち消す。
+  @JsonKey(name: 'redirectUrl')
   final String redirectUrl;
+  @JsonKey(name: 'paytreeCustomerNumber')
   final String? paytreeCustomerNumber;
 
   BankPayRedirectUrl({

lib/responses/bankpay_redirect_url.g.dart
@@ -8,12 +8,12 @@ part of 'bankpay_redirect_url.dart';
 
 BankPayRedirectUrl _$BankPayRedirectUrlFromJson(Map<String, dynamic> json) =>
     BankPayRedirectUrl(
-      redirectUrl: json['redirect_url'] as String,
-      paytreeCustomerNumber: json['paytree_customer_number'] as String?,
+      redirectUrl: json['redirectUrl'] as String,
+      paytreeCustomerNumber: json['paytreeCustomerNumber'] as String?,
     );
 
 Map<String, dynamic> _$BankPayRedirectUrlToJson(BankPayRedirectUrl instance) =>
     <String, dynamic>{
-      'redirect_url': instance.redirectUrl,
-      'paytree_customer_number': instance.paytreeCustomerNumber,
+      'redirectUrl': instance.redirectUrl,
+      'paytreeCustomerNumber': instance.paytreeCustomerNumber,
     };

lib/responses/private_money.dart
@@ -13,6 +13,9 @@ class PrivateMoney {
   final String type;
   final String unit;
   final String description;
+  // フィールド名は oneline (一行) の typo だが、公開 API 互換のため改名せず
+  // JSON キーだけ明示する。field_rename: snake では online_message になってしまう。
+  @JsonKey(name: 'oneline_message')
   final String onlineMessage;
   final String? accountImage;
   final Images images;
@@ -26,6 +29,8 @@ class PrivateMoney {
   final String? paymentActUrl;
   final String? commercialActUrl;
   final bool canUseCreditCard;
+  // field_rename: snake は大文字1文字ごとに _ を挟むため C2C が c2_c になる。
+  @JsonKey(name: 'can_use_c2c_transfer')
   final bool canUseC2CTransfer;
   final String? customDomainName;
   final List<TopupMethod>? topupMethods;

lib/responses/private_money.g.dart
@@ -12,7 +12,7 @@ PrivateMoney _$PrivateMoneyFromJson(Map<String, dynamic> json) => PrivateMoney(
       type: json['type'] as String,
       unit: json['unit'] as String,
       description: json['description'] as String,
-      onlineMessage: json['online_message'] as String,
+      onlineMessage: json['oneline_message'] as String,
       accountImage: json['account_image'] as String?,
       images: Images.fromJson(json['images'] as Map<String, dynamic>),
       organization:
@@ -26,7 +26,7 @@ PrivateMoney _$PrivateMoneyFromJson(Map<String, dynamic> json) => PrivateMoney(
       paymentActUrl: json['payment_act_url'] as String?,
       commercialActUrl: json['commercial_act_url'] as String?,
       canUseCreditCard: json['can_use_credit_card'] as bool,
-      canUseC2CTransfer: json['can_use_c2_c_transfer'] as bool,
+      canUseC2CTransfer: json['can_use_c2c_transfer'] as bool,
       customDomainName: json['custom_domain_name'] as String?,
       topupMethods: (json['topup_methods'] as List<dynamic>?)
           ?.map((e) => TopupMethod.fromJson(e as Map<String, dynamic>))
@@ -40,7 +40,7 @@ Map<String, dynamic> _$PrivateMoneyToJson(PrivateMoney instance) =>
       'type': instance.type,
       'unit': instance.unit,
       'description': instance.description,
-      'online_message': instance.onlineMessage,
+      'oneline_message': instance.onlineMessage,
       'account_image': instance.accountImage,
       'images': instance.images,
       'organization': instance.organization,
@@ -53,7 +53,7 @@ Map<String, dynamic> _$PrivateMoneyToJson(PrivateMoney instance) =>
       'payment_act_url': instance.paymentActUrl,
       'commercial_act_url': instance.commercialActUrl,
       'can_use_credit_card': instance.canUseCreditCard,
-      'can_use_c2_c_transfer': instance.canUseC2CTransfer,
+      'can_use_c2c_transfer': instance.canUseC2CTransfer,
       'custom_domain_name': instance.customDomainName,
       'topup_methods': instance.topupMethods,
     };